cda12ba3a67d212edd0e1e453524c77eed0113dd
Nguyễn An Vĩ <nguyenanvi122333@gmail.com>
2022-08-28 18:06:16 +0700

    cai dat lai cac Handler truy xuat CSDL

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,11 +1,23 @@
-const ApiError = require("../api-error");
 const ContactService = require("../services/contact.service");
 const MongoDB = require("../utils/mongodb.util");
+const ApiError = require("../api-error");
 
-
-exports.create = (req, res) => {
-    res.send({ message: "create handler"});
+exports.create = async(req, res, next) => {
+    // res.send({ message: "create handler"});
+    if(!req.body?.name){
+        return next(new ApiError(400, "Name can not be empty"));
+    }
+    try{
+        const contactService = new ContactService(MongoDB.client);
+        const document = await contactService.create(req.body);
+        return res.send(document);
+    } catch (error){
+        return next(
+            new ApiError(500, "An error occurred while creating the contact")
+        );
+    }
 };
+
 exports.findAll = async(req, res, next) => {
     let documents = [];
 
@@ -24,7 +36,8 @@ exports.findAll = async(req, res, next) => {
     }
     return res.send(documents);
 };
-exports.findOne = (req, res) => {
+
+exports.findOne = async(req, res, next) => {
     // res.send({ message: "findOne handler"});
     try {
         const contactService = new ContactService(MongoDB.client);
@@ -33,14 +46,12 @@ exports.findOne = (req, res) => {
             return next(new ApiError(404, "Contact not found"));
         }
         return res.send(document);
-    } catch (error){
-        return next(
-            new ApiError(500, `Error retrieving contact with id=${req.params.id}`)
-        );
+    } catch (error) {
+        return next(new ApiError(500, `Error retrieving contact with id=${req.params.id}`));
     }
 };
 
-exports.update = (req, res) => {
+exports.update = async(req, res, next) => {
     // res.send({ message: "update handler"});
     if(Object.keys(req.body).length === 0){
         return next(new ApiError(400, "Data to update can not be empty"));
@@ -58,7 +69,7 @@ exports.update = (req, res) => {
     }
 };
 
-exports.delete = (req, res) => {
+exports.delete = async(req, res, next) => {
     // res.send({ message: "delete handler"});
     try{
         const contactService = new ContactService(MongoDB.client);
@@ -66,13 +77,14 @@ exports.delete = (req, res) => {
         if(!document){
             return next(new ApiError(404, "Contact not found"));
         }
-        return req.send({message: "Contact was deleted successfully" });
+        return res.send({message: "Contact was deleted successfully" });
     }catch (error){
+        console.log(error);
         return next(new ApiError(500, `Could not delete contact with id=${req.params.id}`));
     }
 };
 
-exports.deleteAll = (req, res) => {
+exports.deleteAll = async(_req, res, next) => {
     // res.send({ message: "deleteAll handler"});
     try {
         const contactService = new ContactService(MongoDB.client);
@@ -83,7 +95,7 @@ exports.deleteAll = (req, res) => {
     }
 };
 
-exports.findAllFavorite = (req, res) => {
+exports.findAllFavorite = async(req, res, next) => {
     // res.send({ message: "findAllFavorite handler"});
     try {
         const contactService = new ContactService(MongoDB.client);
@@ -91,21 +103,5 @@ exports.findAllFavorite = (req, res) => {
         return res.send(documents);
     } catch (error) {
         return next(new ApiError(500, "An error occured while retrieving favorite contacts"));
-    }
-};
-
-exports.create = async (req, res, next) => {
-    // console.log(req.body);
-    if (!req.body?.name) {
-        return next(new ApiError(400, "Name can not be empty"));
-    }
-    try {
-        const contactService = new ContactService(MongoDB.client);
-        const document = await contactService.create(req.body);
-        return res.send(document);
-    } catch (error) {
-        return next(
-            new ApiError(500, "An error occurred while creating the contact")
-        );
     }
 };
